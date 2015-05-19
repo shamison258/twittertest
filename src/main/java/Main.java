68146feab653cca5e49f5423a2b3b89cab61d78a
shamison <unknown0incident@gmail.com>
2015-05-19 17:31:31 +0900

    add tweet streaming

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import twitter4j.Status;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.UserStreamAdapter;
+
+/**
+ * Created by shamison on 15/05/19.
+ */
+public class Main {
+	public static void main(String[] args) {
+		TwitterStream twitter = new TwitterStreamFactory().getInstance();
+		twitter.addListener(new Listener());
+		twitter.user();
+	}
+
+	static class Listener extends UserStreamAdapter {
+		@Override
+		public void onStatus(Status status) {
+			super.onStatus(status);
+			System.out.println(
+					status.getUser().getName()
+							+ "(@" + status.getUser().getScreenName() + ")"
+							+ ": "
+							+ status.getText()
+			);
+		}
+	}
+}
